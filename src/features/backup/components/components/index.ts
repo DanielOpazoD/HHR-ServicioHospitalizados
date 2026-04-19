660189b52d0bd8f2930ef89c1cce61ece82b8173
Daniel Opazo <daniel.opazo@hospitalhangaroa.cl>
2026-04-19 14:26:26 -0600

    refactor: remove unused component barrels

--- src/features/backup/components/components/index.ts
+++
@@ -1,4 +0,0 @@
-// Backup components barrel export
-export { BackupFileCard } from './BackupFileCard';
-export { BackupFilters } from './BackupFilters';
-export { BackupPreviewModal } from './BackupPreviewModal';
