edc895f80f02995ef87598bebc27a6e46c8be55b
DanielOpazoD <d.opazo.damiani@gmail.com>
2026-05-10 13:45:28 -0600

    Fix new-admission indicator and future same-bed copy

--- src/components/modals/actions/MoveCopyModal.tsx
+++ src/components/modals/actions/MoveCopyModal.tsx
@@ -66,6 +66,7 @@ export const MoveCopyModal: React.FC<MoveCopyModalProps> = ({
             allBeds: BEDS,
             currentRecord,
             targetRecord: resolvedTargetRecord,
+            selectedDate,
             sourceBedId,
             targetBedId,
           })
